2767eccc39b8ebc8536e978a967079afe0015da0
Ayesumoon <xadnxch.17@gmail.com>
2025-11-23 23:26:18 +0800

    changes 11/23

--- orders.php
+++ orders.php
@@ -389,8 +389,8 @@ body {
 
 
           <td class="px-4 py-2">
-            <span class="px-2 py-1 rounded text-xs font-semibold bg-gray-100 border border-gray-300">
-              <?= htmlspecialchars($order['order_status']); ?>
+            <span class="px-2 py-1 rounded text-xs font-semibold bg-green-100 text-green-600 border border-green-200">
+              Completed
             </span>
           </td>
 
